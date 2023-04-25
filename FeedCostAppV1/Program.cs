5729b9fb4756c4fac6c2787d84647d9c30437618
JaxMcG <99847329+JaxMcG@users.noreply.github.com>
2023-04-26 09:15:23 +1200

    Add project files.

---
+++ FeedCostAppV1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FeedCostAppV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
